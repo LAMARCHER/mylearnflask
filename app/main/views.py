f7452e19bc7ab892494ba523fea1d267ae15baca
weibing.liu <weibing.liu@qunar.com>
2018-01-20 22:18:03 +0800

    'add'

--- app/main/views.py
+++ app/main/views.py
@@ -8,7 +8,7 @@ from .forms import NameForm, EditProfileForm, EditProfileAdminForm
 from app.decorators import admin_required, permission_required
 from app.models import Permission
 from .. import db
-from app.models import User
+from app.models import User, Role
 
 
 @main.route('/', methods=['GET', 'POST'])
@@ -106,3 +106,22 @@ def edit_profile():
 def edit_profile_admin(id):
     user = User.query.get_or_404(id)
     form = EditProfileAdminForm(user=user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role = Role.query.get(form.role.data)
+        user.name = form.name.data
+        user.location = form.location.data
+        user.about_me = form.about_me.data
+        db.session.add(user)
+        flash('The profile has been updated.')
+        return redirect(url_for('.user', username=user.username))
+    form.email.data = user.email
+    form.username.data = user.username
+    form.confirmed.data = user.confirmed
+    form.role.data = user.role_id
+    form.name.data = user.name
+    form.location.data = user.location
+    form.about_me.data = user.about_me
+    return render_template('edit_profile.html', form=form, user=user)
--- app/models.py
+++ app/models.py
@@ -1,10 +1,11 @@
 # coding:utf-8
 import datetime
+import hashlib
 
 from flask_login import UserMixin, AnonymousUserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask import current_app
+from flask import current_app, request
 
 from app import db
 from . import login_manager
@@ -61,6 +62,7 @@ class User(UserMixin, db.Model):
     about_me = db.Column(db.Text())
     member_since = db.Column(db.DateTime(), default=datetime.datetime.utcnow)
     last_seen = db.Column(db.DateTime(), default=datetime.datetime.utcnow)
+    avatar_hash = db.Column(db.String(32))
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -100,6 +102,8 @@ class User(UserMixin, db.Model):
                 self.role = Role.query.filter_by(permissions=0xff).first()
             if self.role is None:
                 self.role = Role.query.filter_by(default=True).first()
+            if self.email is not None and self.avatar_hash is None:
+                self.avatar_hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
 
     def can(self, permissions):
         return self.role is not None and (self.role.permissions & permissions) == permissions
@@ -110,6 +114,24 @@ class User(UserMixin, db.Model):
     def ping(self):
         self.last_seen = datetime.datetime.utcnow()
         db.session.add(self)
+
+    def gravatar(self, size=100, default='identicon', rating='g'):
+        if request.is_secure:
+            url = 'https://secure.gravatar.com/avatar'
+        else:
+            url = 'http://www.gravatar.com/avatar'
+        hash = self.avatar_hash or hashlib.md5(self.email.encode('utf-8')).hexdigest()
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
+            url=url, hash=hash, size=size, default=default, rating=rating
+        )
+
+    def change_email(self, token, new_email):
+        self.email = new_email
+        self.avatar_hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
+        db.session.add(self)
+        return True
+
+
 
 
 class AnonymousUser(AnonymousUserMixin):
